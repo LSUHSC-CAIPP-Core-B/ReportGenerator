564516bf4cea86bb15043159f11ea6a18a68eb88
Leialoha <github@leialoha.dev>
2026-01-20 19:16:52 -0600

    feat(frontend): implement LiquidJS view engine

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
-import express, { type Request, type Response } from 'express';
+import path from 'node:path';
+import express, { type Request, type Response, static as staticFiles } from 'express';
+import { Liquid } from 'liquidjs'
 
 const server = express();
+const liquid = new Liquid({ extname: '.html' });
+
+server.engine('html', liquid.express());
+server.set('views', path.resolve('templates'));
+server.set('view engine', 'liquid');
+server.use('/assets', staticFiles('templates/assets'));
 
 server.get('/', (req: Request, res: Response) => {
-  res.send('Hello world!');
+  res.render('builtin/base.html', {
+    timestamp: (new Date()).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' }),
+    report: {
+      title: "test"
+    }
+  });
 });
 
 export default server;
