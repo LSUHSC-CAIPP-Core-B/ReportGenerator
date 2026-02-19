2fbd10f8cc52a827bf85aeaa05ecf044beeddab6
Leialoha <github@leialoha.dev>
2026-02-19 16:03:21 -0600

    ci: implement `patch` types for conventional commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,4 @@
-module.exports = { extends: ['@commitlint/config-conventional'] };
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    "type-enum": [ "build", "chore", "ci", "docs", "feat", "fix", "patch", "perf", "refactor", "revert", "style", "test" ]
+};
